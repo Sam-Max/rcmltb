5578c98bb4c6573d63e4bc02c4d556f767191c9d
SamAgd <sam.agd@outlook.com>
2022-02-09 14:41:34 -0500

    update

--- bot/utils/get_rclone.py
+++ bot/utils/get_rclone.py
@@ -6,6 +6,7 @@ async def get_rclone():
     path = os.path.join(os.getcwd(), "rclone.conf")
 
     rclone_config= SessionVars.get_var("RCLONE_CONFIG")
+    rclone_config.strip()
     str_1_encoded = bytes(rclone_config,'UTF-8')
 
     with open(path, "wb") as rfile:
