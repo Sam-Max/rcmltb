ef97011dcbb08e6a765554e334899f60b79f50ab
SamAgd <sam.agd@outlook.com>
2022-02-18 07:57:21 +0100

    update list function

--- bot/utils/list_selected_drive.py
+++ bot/utils/list_selected_drive.py
@@ -8,14 +8,13 @@ log = logging.getLogger(__name__)
 yes = "✅"
 folder = "📁"
 
-
 async def list_selected_drive(drive_base, drive_name, conf_path, rclone_dir, data_cb, menu):
     prev="" 
     if get_val("BASE_DIR") == "/":
         prev= yes
     menu.append([KeyboardButtonCallback(f"{prev} Seleccione esta Carpeta", f"settings {data_cb} / )".encode("UTF-8"))])
 
-    cmd = ["rclone", "lsjson", f'--config={conf_path}', f"{drive_name}:{drive_base}", "--dirs-only"]
+    cmd = ["rclone", "lsjson", f'--config={conf_path}', f"{drive_name}:{drive_base}"]
 
     # piping only stdout
     process = await asyncio.create_subprocess_exec(
@@ -28,19 +27,24 @@ async def list_selected_drive(drive_base, drive_name, conf_path, rclone_dir, dat
 
     try:
         data = json.loads(stdout)
+        log.info(data)
         for i in data:
             path = i["Path"]
             path == path.strip()
-            size = i["Size"]
+            mime_type= i['MimeType']
             prev2= ""   
             folder1= folder
-            if len(path) <= 20 and size == -1:
+            zip= ""
+            if len(path) <= 20: 
                 if path == rclone_dir:
                     prev2= yes
+                    folder1= ""
+                if mime_type == 'application/zip':  
+                    zip= "🗄"  
                     folder1= ""
                 if " " in path:
                     continue    
                 menu.append(
-                    [KeyboardButtonCallback(f"{prev2} {folder1} {path}", f"settings {data_cb} {path}".encode("UTF-8"))])
+                    [KeyboardButtonCallback(f"{prev2} {folder1} {zip} {path}", f"settings {data_cb} {path}".encode("UTF-8"))])
     except Exception as e:
         log.info(e)
