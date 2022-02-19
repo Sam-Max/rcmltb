125b9ddcd0621a8389ac92fed99092adbd9de758
SamAgd <sam.agd@outlook.com>
2022-02-19 17:41:25 +0100

    update start command

--- bot/core/HandleManager.py
+++ bot/core/HandleManager.py
@@ -298,7 +298,11 @@ async def get_logs_f(e):
 
 async def start_handler(e):
     if await is_admin(e.sender_id):
-        msg = "Hola bienvenido. Disfrute de este bot."
+        msg = ''' **Hola, ¡Bienvenido a Rclone bot!**\n
+Puedo ayudarte a transferir archivos de una nube a otra nube. También puedo subir archivos de telegram a su almacenamiento en la nube.\n
+**Se admiten más de 40 tipos de almacenamiento en la nube.**\n
+'''
+        #e.client.parse_mode = 'html'
         await e.reply(msg)
     else:
         await e.delete()
