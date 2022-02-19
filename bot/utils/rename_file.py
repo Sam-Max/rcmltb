e9b64454a88787c57fc07919a17d96a724e0a6a3
SamAgd <sam.agd@outlook.com>
2022-02-19 17:03:29 +0100

    update rename_file

--- bot/utils/rename_file.py
+++ bot/utils/rename_file.py
@@ -1,12 +1,13 @@
 import os
 
-async def rename(path, new_name):
-    os.chdir("Downloads")
-    file_name, file_extension = os.path.splitext(path)
-    name = file_name.split("/")[-1]
-    old_name= name + file_extension
+async def rename(old_path, new_name):
+    #os.chdir("Downloads")
+    file_name, file_extension = os.path.splitext(old_path)
+    #name = file_name.split("/")[-1]
+    #old_name= name + file_extension
     new_name= new_name + file_extension
-    os.rename(old_name, new_name)
-    path = os.path.join(os.getcwd(), new_name) 
-    return path
+    new_path= os.path.join("Downloads", new_name)
+    os.rename(old_path, new_path)
+    #path = os.path.join(os.getcwd(), new_name) 
+    return new_path
     
