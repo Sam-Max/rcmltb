8258e3b6e960b7623b2cc94ae77a44a334c39614
SamAgd <sam.agd@outlook.com>
2022-02-20 10:06:05 +0100

    fix

--- bot/utils/rename_file.py
+++ bot/utils/rename_file.py
@@ -8,7 +8,7 @@ async def rename(old_path, new_name):
     #old_name= name + file_extension
     new_name= new_name + file_extension
     new_path= os.path.join(os.getcwd(), "Downloads", new_name)
-    logging.info(os.getcwd())
+    logging.info(old_path)
     logging.info(new_path)
     os.rename(old_path, new_path)
     #path = os.path.join(os.getcwd(), new_name) 
