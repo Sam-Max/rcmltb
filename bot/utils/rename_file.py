caa03914a6016597ce8576951b558e514a194f84
SamAgd <sam.agd@outlook.com>
2022-02-20 09:37:08 +0100

    fix

--- bot/utils/rename_file.py
+++ bot/utils/rename_file.py
@@ -6,7 +6,7 @@ async def rename(old_path, new_name):
     #name = file_name.split("/")[-1]
     #old_name= name + file_extension
     new_name= new_name + file_extension
-    new_path= os.path.join("Downloads", new_name)
+    new_path= os.path.join(os.getcwd, "Downloads", new_name)
     os.rename(old_path, new_path)
     #path = os.path.join(os.getcwd(), new_name) 
     return new_path
