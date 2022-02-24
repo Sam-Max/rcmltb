ec2ceb759cb76b4aa2134426871e9318ebce3d56
SamAgd <sam.agd@outlook.com>
2022-02-24 10:04:06 +0100

    update config.py

--- bot/config/config.py
+++ bot/config/config.py
@@ -1,5 +1,4 @@
 
-import os
 
 class ExecConfig:
 
@@ -9,34 +8,25 @@ class ExecConfig:
 
         OWNER_ID = 0
 
-        # Chracter to use as a completed progress 
         COMPLETED_STR = "▰"
 
-        # Chracter to use as a incomplete progress
         REMAINING_STR = "▱"
 
         BASE_DIR= "/"
+        ORIGIN_DIR= "/"
+        DEST_DIR= "/"
 
-        # Set this value to show all the remotes while leeching
-        SHOW_REMOTE_LIST = False
-
-        # to determine if upload process is canceled        
         UP_CANCEL= False
 
-        # For vps set path here or you can use runtime too
-        #RCLONE_CONFIG= os.path.join(os.getcwd(), 'rclone.conf')
+        # ---For vps set path here--
+        # RCLONE_CONFIG= os.path.join(os.getcwd(), 'rclone.conf')
 
-        #rclone config text from conf file
         RCLONE_CONFIG = ""
         
-        # Name of the RCLONE drive from the config
         DEF_RCLONE_DRIVE = ""
 
-        # Time to wait before edit message
         EDIT_SLEEP_SECS = 10
 
-        # Set this to your bot username if you want to add the username of your bot at the end of the commands like
-        # /leech@Bot so the value will be @Bot
         BOT_CMD_POSTFIX = "" 
 
         
