121e9c3bb659fb7f60c9b8d0a36ad8b871447e10
SamAgd <sam.agd@outlook.com>
2022-03-04 17:32:09 +0100

    refactor rclone upload

--- bot/downloaders/telegram_download.py
+++ bot/downloaders/telegram_download.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from bot.core.get_vars import get_val
 from pyrogram.errors.exceptions.bad_request_400 import MessageNotModified
 from bot.utils.get_rclone_conf import get_config
-from ..uploaders.rclone_upload import RcloneUploader
+from ..uploaders.rclone_upload import rclone_uploader
 from bot.downloaders.progress_for_pyrogram import progress_for_pyrogram
 
 logging.basicConfig(
@@ -18,38 +18,45 @@ LOGGER = logging.getLogger(__name__)
 
 async def down_load_media_pyro(client, message, message_type, new_name= None, is_rename= False):
         mess_age = await message.reply_text("...", quote=True)
+        
         LOGGER.info("downloading...")
         conf_path = await get_config()
+        
         if conf_path is None:
             await mess_age.edit("Rclone config file not found.")
             return
         dest_drive = get_val("DEF_RCLONE_DRIVE")
+        
         if dest_drive == "":
             await mess_age.edit("Select a cloud first please")
             return      
+        
         start_t = datetime.now()
         path = os.path.join(os.getcwd(), "Downloads")
         path = path + "/"
         c_time = time.time()
-        the_real_download_location = await client.download_media(
+        
+        download_location = await client.download_media(
             message=message_type,
             file_name=path,
             progress=progress_for_pyrogram,
             progress_args=(
-               "Downloading...", mess_age, c_time
+               "Downloading...",
+                mess_age, 
+                c_time
             )
         )
         end_t = datetime.now()
         ms = (end_t - start_t).seconds
-        print(the_real_download_location)
 
         try:
             await mess_age.edit(text= f"Downloaded in <u>{ms}</u> seconds")
         except MessageNotModified as e:
+            LOGGER.info(e)
             pass
+
+        await rclone_uploader(download_location, mess_age, new_name, is_rename)
         
-        rclone_up = RcloneUploader(the_real_download_location, mess_age, new_name, is_rename= is_rename)
-        await rclone_up.execute()
 
 
 
--- bot/uploaders/rclone_upload.py
+++ bot/uploaders/rclone_upload.py
@@ -14,107 +14,63 @@ from .progress_for_rclone import status
 
 log = logging.getLogger(__name__)
 
+async def rclone_uploader(path, user_msg, new_name, is_rename= False):
 
-class RcloneUploader():
+    rclone_pr = None
+    dest_base= None
+    old_path = path
+    dest_drive = get_val("DEF_RCLONE_DRIVE")
 
-    def __init__(self, path, user_msg, new_name, is_rename= False):
-        super().__init__()
-        self._path = path
-        self._is_rename= is_rename
-        self._user_msg = user_msg
-        self._new_name= new_name
-        self._rclone_pr = None
-        self.dest_base= None
+    conf_path = await get_config()
+    conf = ConfigParser()
+    conf.read(conf_path)
+    gen_drive_name = ""
 
-    async def execute(self):
-        old_path = self._path
-        new_name= self._new_name
-        is_rename= self._is_rename
+    for i in conf.sections():
+        if dest_drive == str(i):
+            if conf[i]["type"] == "drive":
+                dest_base = get_val("BASE_DIR")
+                log.info("Google Drive Upload Detected.")
+            else:
+                gen_drive_name = conf[i]["type"]
+                dest_base = get_val("BASE_DIR")
+                log.info(f"{gen_drive_name} Upload Detected.")
+            break
 
-        dest_drive = get_val("DEF_RCLONE_DRIVE")
+    if not os.path.exists(old_path):
+        await user_msg.reply("the path {path} not found")
+        return 
 
-        conf_path = await get_config()
-        conf = ConfigParser()
-        conf.read(conf_path)
-        general_drive_name = ""
-
-        for i in conf.sections():
-            if dest_drive == str(i):
-                if conf[i]["type"] == "drive":
-                    self.dest_base = get_val("BASE_DIR")
-                    log.info("Google Drive Upload Detected.")
-                else:
-                    general_drive_name = conf[i]["type"]
-                    self.dest_base = get_val("BASE_DIR")
-                    log.info(f"{general_drive_name} Upload Detected.")
-                break
-
-        if not os.path.exists(old_path):
-            await self._user_msg.reply("the path {path} not found")
-            return 
-
-        if is_rename:
-            path = await rename(old_path, new_name)
-        else:
-            path= old_path    
+    if is_rename:
+        path = await rename(old_path, new_name)
+    else:
+        path= old_path    
             
-        if os.path.isdir(path):
-            new_dest_base = self.dest_base
-            logging.info(new_dest_base)
-            rclone_copy_cmd = ['rclone', 'copy', f'--config={conf_path}', str(path),
-                                    f'{dest_drive}:{new_dest_base}', '-P']
+    rclone_copy_cmd = ['rclone', 'copy', f'--config={conf_path}', str(path),
+                            f'{dest_drive}:{dest_base}', '-P']
 
-            
-            log.info(f'{dest_drive}:{new_dest_base}')
-            log.info("Uploading...")
+    log.info("Uploading...")
 
-            rclone_pr = subprocess.Popen(
-                rclone_copy_cmd,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE
-            )
+    rclone_pr = subprocess.Popen(
+        rclone_copy_cmd,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
 
-            self._rclone_pr = rclone_pr
-            rcres= await self.rclone_process_update()
+    rcres= await rclone_process_update(rclone_pr, user_msg)
 
-            if rcres:
-                rclone_pr.kill()
-                await self._user_msg.edit("Upload cancelled")
-                return 
-            
-            await self._user_msg.edit("Successfully uploaded ✅")
+    if rcres:
+        rclone_pr.kill()
+        await user_msg.edit("Upload cancelled")
+        return 
+    
+    log.info("Successfully uploaded")
+    await user_msg.edit("Successfully uploaded ✅")
 
-        else:
-            new_dest_base = self.dest_base
-            logging.info(new_dest_base)
-            rclone_copy_cmd = ['rclone', 'copy', f'--config={conf_path}', str(path),
-                                    f'{dest_drive}:{new_dest_base}', '-P']
-            
-            log.info(f'{dest_drive}:{new_dest_base}')
-            log.info("Uploading...")
-
-            rclone_pr = subprocess.Popen(
-                rclone_copy_cmd,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE
-            )
-
-            self._rclone_pr = rclone_pr
-            rcres= await self.rclone_process_update()
-
-            if rcres:
-                rclone_pr.kill()
-                await self._user_msg.edit("Upload cancelled")
-                return 
-            
-            log.info("Successfully uploaded")
-            await self._user_msg.edit("Successfully uploaded ✅")
-
-
-    async def rclone_process_update(self):
+async def rclone_process_update(rclone_pr, user_msg):
         blank=0    
-        process = self._rclone_pr
-        user_message = self._user_msg
+        process = rclone_pr
+        user_message = user_msg
         sleeps = False
         msg = ""
         msg1 = ""
