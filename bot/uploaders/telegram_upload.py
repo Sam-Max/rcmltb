63facd16472cb93690953b58486347cb51d22a82
samagd <sam.agd@outlook.com>
2022-03-07 10:50:42 +0100

    update

--- bot/uploaders/telegram_upload.py
+++ bot/uploaders/telegram_upload.py
@@ -26,8 +26,7 @@ async def upload_media_pyro(client, message, sender, file):
                         path = str(file).split(".")[0] + ".mp4"
                         os.rename(file, path) 
                         file = str(file).split(".")[0] + ".mp4"
-                    caption= str(file).split("/")[-1]   
-                    logging.info(caption) 
+                        caption= str(file).split("/")[-1]   
                     data = video_metadata(file)
                     duration = data["duration"]
                     thumb_path = await screenshot(file, duration, sender)
@@ -49,10 +48,16 @@ async def upload_media_pyro(client, message, sender, file):
                             c_time
                         )
                     )
+            # elif str(file).split(".")[-1] in ['jpg', 'jpeg', 'png', 'webp']:
+            #         await edit.edit("Uploading photo.")
+            #         await bot.send_file(sender, file, caption=caption)  
+            # elif str(file).split(".")[-1] in ['docx', 'doc', 'png', 'webp']:
+            #         await edit.edit("Uploading document.")
+            #         await bot.send_file(sender, file, caption=caption)        
             else:
                 await client.send_document(
                     chat_id= sender,
-                    document= str(file), 
+                    document= file, 
                     progress=progress_for_pyrogram,
                     progress_args=(
                         '**Uploading:**',
