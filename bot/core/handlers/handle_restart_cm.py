dc745918b7c4787dd073926a56aa73a69792123d
samagd <sam.agd@outlook.com>
2022-03-11 10:14:30 +0100

    update restart

--- bot/core/handlers/handle_restart_cm.py
+++ bot/core/handlers/handle_restart_cm.py
@@ -10,15 +10,15 @@ log = logging.getLogger(__name__)
 async def handle_restart(e):
     update_message= await e.reply("Restarting...")
 
+    srun(["python3", "update.py"])
+
     try:
         for line in os.popen("ps ax | grep " + "rclone" + " | grep -v grep"):
             fields = line.split()
             pid = fields[0]
             os.kill(int(pid), signal.SIGKILL)
-    except:
-        log.info("Error")
-
-    srun(["python3", "update.py"])
+    except Exception as exc:
+        log.info(f"Error: {exc}")
 
     with open(".updatemsg", "w") as f:
         f.truncate(0)
