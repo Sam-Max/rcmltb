f96e4ef5920906b856d3830d075871dc307b731f
samagd <sam.agd@outlook.com>
2022-03-18 04:49:30 +0100

    update handle_config_cm.py

--- bot/core/handlers/handle_config_cm.py
+++ bot/core/handlers/handle_config_cm.py
@@ -1,9 +1,11 @@
+import logging
 from bot.core.settings_main_menu import settings_main_menu
 from bot.utils.admin_check import is_admin
 
 
 async def handle_config_command(e):
     if await is_admin(e.sender_id):
+        logging.info(e)     
         await settings_main_menu(e)
     else:
         await e.delete()    
