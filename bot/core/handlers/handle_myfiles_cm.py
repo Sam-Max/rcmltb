3599a51b91a2ea375926caf7e05a54833c73b026
samagd <sam.agd@outlook.com>
2022-03-31 22:33:06 +0200

    add permission check on myfiles handler

--- bot/core/handlers/handle_myfiles_cm.py
+++ bot/core/handlers/handle_myfiles_cm.py
@@ -1,10 +1,16 @@
+from bot.core.get_vars import get_val
 from bot.core.menus.menu_myfiles import settings_myfiles_menu
 
 
 async def handle_myfiles(client, message):
-     await settings_myfiles_menu(
+    user_id= message.from_user.id
+    chat_id = message.chat.id
+    if user_id in get_val("ALLOWED_USERS") or chat_id in get_val("ALLOWED_CHATS") or user_id == get_val("OWNER_ID"):
+         await settings_myfiles_menu(
                 client= client, 
                 message= message,
                 msg= "Please select your drive to see files", 
                 data_cb="list_drive_myfiles_menu"
-            ) 
+            )
+    else:
+        await message.reply('Not Authorized user', quote=True)
