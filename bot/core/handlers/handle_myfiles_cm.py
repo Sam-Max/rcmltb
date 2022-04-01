2ab24427ac3b74e98a1f61cf1807e629a970d48d
samagd <sam.agd@outlook.com>
2022-04-01 11:54:54 +0200

    update handle_myfiles_cm.py

--- bot/core/handlers/handle_myfiles_cm.py
+++ bot/core/handlers/handle_myfiles_cm.py
@@ -1,12 +1,12 @@
 from bot.core.get_vars import get_val
-from bot.core.menus.menu_myfiles import settings_myfiles_menu
+from bot.core.menus.menu_myfiles import myfiles_menu
 
 
 async def handle_myfiles(client, message):
     user_id= message.from_user.id
     chat_id = message.chat.id
     if user_id in get_val("ALLOWED_USERS") or chat_id in get_val("ALLOWED_CHATS") or user_id == get_val("OWNER_ID"):
-         await settings_myfiles_menu(
+         await myfiles_menu(
                 client= client, 
                 message= message,
                 msg= "Please select your drive to see files", 
