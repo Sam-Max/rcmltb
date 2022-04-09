0493fd465f32ce1918495161aa2589c7280bdad3
samagd <sam.agd@outlook.com>
2022-04-09 09:29:16 +0200

    minor edit

--- bot/core/handlers/handle_config_cm.py
+++ bot/core/handlers/handle_config_cm.py
@@ -1,9 +1,11 @@
+from bot.core.get_vars import get_val
 from bot.core.menus.main_menu import settings_main_menu
-from bot.utils.admin_check import is_admin
 
 
-async def handle_config_command(e):
-    if await is_admin(e.sender_id):
-        await settings_main_menu(e)
+async def handle_config_command(message):
+    user_id= message.sender_id
+    chat_id= message.chat_id
+    if user_id in get_val("ALLOWED_USERS") or chat_id in get_val("ALLOWED_CHATS") or user_id == get_val("OWNER_ID"):
+        await settings_main_menu(message)
     else:
-        await e.reply('Not Authorized user') 
+        await message.reply('Not Authorized user') 
