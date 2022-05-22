18d2583f70d7db874a5c42579416b8498fcf6668
samagd <sam.agd@outlook.com>
2022-05-22 20:17:53 +0200

    update misc_utils.py

--- bot/utils/misc_utils.py
+++ bot/utils/misc_utils.py
@@ -4,16 +4,17 @@ import shutil
 
 log = logging.getLogger(__name__)
 
-def clean_all():
-    try:
-        shutil.rmtree("./Downloads")
-    except:
-        pass
-
-def clean_download(path: str):
-     log.info("Cleaning path {}".format(path))
-     if os.path.exists(path):
+def clean_path(path):
+    if os.path.exists(path):
+        log.info(f"Cleaning Download: {path}")
         try:
-            shutil.rmtree(path)
+           shutil.rmtree(path)
         except:
             pass
+
+def clean_filepath(file_path):
+     log.info(f"Cleaning Download: {file_path}")
+     try:
+        os.remove(file_path)
+     except:
+        pass
