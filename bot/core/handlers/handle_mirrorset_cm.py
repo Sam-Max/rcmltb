8af727782b3cc83671e483a0f8c2e7c8907cee09
samagd <sam.agd@outlook.com>
2022-05-23 02:59:00 +0200

    refactor & renaming

--- bot/core/handlers/handle_mirrorset_cm.py
+++ bot/core/handlers/handle_mirrorset_cm.py
@@ -1,11 +1,11 @@
 from bot.core.get_vars import get_val
-from bot.core.menus.main_menu import settings_main_menu
+from bot.core.menus.menu_mirrorset import settings_mirrorset_menu
 
 
 async def handle_mirrorset_command(message):
     user_id= message.sender_id
     chat_id= message.chat_id
     if user_id in get_val("ALLOWED_USERS") or chat_id in get_val("ALLOWED_CHATS") or user_id == get_val("OWNER_ID"):
-        await settings_main_menu(message)
+        await settings_mirrorset_menu(message)
     else:
         await message.reply('Not Authorized user') 
