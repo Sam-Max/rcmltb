9f326c03cf0a2a98517cc4795aec69ecce029d45
samagd <sam.agd@outlook.com>
2022-06-01 08:28:04 +0200

    update mega_download.py

--- bot/downloaders/mega/mega_download.py
+++ bot/downloaders/mega/mega_download.py
@@ -1,10 +1,14 @@
+import shutil
 from subprocess import Popen
-from bot import LOGGER
+import time
+
+from psutil import cpu_percent, virtual_memory
+from bot import DOWNLOAD_DIR, LOGGER, uptime
 from bot.core.get_vars import get_val
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from bot.utils import human_format
 from bot.utils.human_format import human_readable_bytes
 from megasdkrestclient import MegaSdkRestClient, errors, constants
-#from megasdkrestclient.constants.State import TYPE_STATE_CANCELED, TYPE_STATE_FAILED, TYPE_STATE_COMPLETED
 import asyncio
 import os
 from functools import partial
@@ -74,33 +78,26 @@ class MegaDownloader():
 
     async def execute(self):
         mega_client = await self.init_mega_client()
-        path = os.path.join(os.getcwd(), "Downloads", "")
-        
         try:
-            dl_add_info = await self._aloop.run_in_executor(None, partial(mega_client.addDl, self._link, path))
+            dl_add_info = await self._aloop.run_in_executor(None, partial(mega_client.addDl, self._link, DOWNLOAD_DIR))
         except errors.MegaSdkRestClientException as e:
-            self._error_reason = str(dict(e.message)["message"]).title()
-            self._is_errored = True
-            return
-        
+            error_reason = str(dict(e.message)["message"]).title()
+            return False, error_reason, None
+
+        LOGGER.info("MegaDL Task Running....")
         self._gid  = dl_add_info["gid"]
-        
-        dl_info = await self._aloop.run_in_executor(None, partial(mega_client.getDownloadInfo, dl_add_info["gid"]))
-        
+        dl_info = await self._aloop.run_in_executor(None, partial(mega_client.getDownloadInfo,dl_add_info["gid"]))
         self._path = os.path.join(dl_add_info["dir"], dl_info["name"])
-        
-        self._update_info = dl_info
 
         while True:
             dl_info = await self._aloop.run_in_executor(None, partial(mega_client.getDownloadInfo, dl_add_info["gid"]))
             
             if dl_info["state"] not in [constants.State.TYPE_STATE_CANCELED, constants.State.TYPE_STATE_FAILED]:
                 if dl_info["state"] == constants.State.TYPE_STATE_COMPLETED:
-                    self._is_completed = True
-                    self._error_reason = "Download Complete."
+                    error_reason = "Download Complete."
                     await asyncio.sleep(2)
-                    return self._path
-                
+                    return True, error_reason, self._path
+
                 try:
                     self._update_info = dl_info
                     await self.aria_progress_update()
@@ -108,18 +105,16 @@ class MegaDownloader():
                     LOGGER.info(e)
             else:
                 if dl_info["state"] == constants.State.TYPE_STATE_CANCELED:
-                    self._is_canceled = True
-                    self._error_reason = "Canceled by user."
-                    await self._user_message.edit("Mega Cancelled")
+                    error_reason = "Mega canceled"
+                    return False, error_reason, None
                 else:
-                    self._is_errored = True
-                    self._error_reason = dl_info["error_string"]
-                return False
+                    error_reason = dl_info["error_string"]
+                return False, error_reason, None
 
     async def aria_progress_update(self):
         update_message1= ""
         sleeps= False
-        LOGGER.info("MegaDL Task Running....")
+        
         if self._update_info is not None:
             sleeps = True
             update_message = await self.create_update_message()
@@ -129,8 +124,7 @@ class MegaDownloader():
                     await self._user_message.edit(text=update_message, reply_markup=(InlineKeyboardMarkup([
                                             [InlineKeyboardButton('Cancel', callback_data=data.encode("UTF-8"))]
                                             ])))
-               except Exception as e:
-                    LOGGER.info("Not expected {}".format(e))
+               except Exception:
                     pass
                if sleeps:
                     sleeps = False
@@ -138,14 +132,22 @@ class MegaDownloader():
                     
     async def create_update_message(self):
         update= self._update_info
-        msg = "<b>Downloading:</b> {}\n".format(update["name"])
-        msg += "<b>Speed:</b> {}\n".format(human_readable_bytes(update["speed"]))
-        msg += "<b>Progress:</b> {} - {}%\n".format(self.progress_bar((update["completed_length"]/update["total_length"])),
-            round((update["completed_length"]/update["total_length"])*100, 2))
+        bottom_status= ''
+        diff = time.time() - uptime
+        diff = human_format.human_readable_timedelta(diff)
+        usage = shutil.disk_usage("/")
+        free = human_format.human_readable_bytes(usage.free) 
+        bottom_status += f"\n<b>CPU:</b> {cpu_percent()}% | <b>FREE:</b> {free}" + f"\n<b>RAM:</b> {virtual_memory().percent}% | <b>UPTIME:</b> {diff}"
+
+        msg =  "<b>Name:</b> {}\n".format(update["name"])
+        msg += "<b>Status:</b> Downloading...\n"
+        msg += "<b>Progress:</b> {}\n".format(self.progress_bar((update["completed_length"]/update["total_length"])))
+        msg += "<b>P:</b> {}%\n".format(round((update["completed_length"]/update["total_length"])*100, 2))
         msg += "<b>Downloaded:</b> {} of {}\n".format(human_readable_bytes(update["completed_length"]),
             human_readable_bytes(update["total_length"]))
-        msg += "<b>ETA:</b> <b>N/A</b>\n"
-        msg += "<b>Using engine:</b> Mega DL"
+        msg += "<b>Speed:</b> {}".format(human_readable_bytes(update["speed"])) + "|" + "<b>ETA:</b> <b>N/A</b>\n"
+        msg += "<b>Using engine:</b> Mega\n"
+        msg += bottom_status
         return msg
 
     def progress_bar(self, percentage):
