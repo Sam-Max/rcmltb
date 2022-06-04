f6705d71d0c3349cbc845ee65de05b0b94a08e46
samagd <sam.agd@outlook.com>
2022-06-04 18:26:58 +0200

    update human_format.py

--- bot/utils/human_format.py
+++ bot/utils/human_format.py
@@ -2,6 +2,19 @@
 
 from datetime import timedelta
 
+SIZE_UNITS = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
+
+def get_readable_file_size(size_in_bytes) -> str:
+    if size_in_bytes is None:
+        return '0B'
+    index = 0
+    while size_in_bytes >= 1024:
+        size_in_bytes /= 1024
+        index += 1
+    try:
+        return f'{round(size_in_bytes, 2)}{SIZE_UNITS[index]}'
+    except IndexError:
+        return 'File too large'
 
 def human_readable_bytes(value, digits=2, delim="", postfix=""):
     """Return a human-readable file size.
