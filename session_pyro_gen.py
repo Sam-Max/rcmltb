b8eddd0f0c5b8dbe9e7e3893e6ea9ce4f7b763e4
samagd <sam.agd@outlook.com>
2022-07-19 16:32:49 +0200

    update session_pyro_gen.py

--- session_pyro_gen.py
+++ session_pyro_gen.py
@@ -1,7 +1,9 @@
 from pyrogram import Client
 
 print('Required pyrogram V2 or greater.')
+
 API_KEY = int(input("Enter API KEY: "))
 API_HASH = input("Enter API HASH: ")
+
 with Client(session_name='USS', api_id=API_KEY, api_hash=API_HASH) as app:
     print(app.export_session_string())
